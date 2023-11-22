2fee094fca70addeeb5621ac4960033a3adcc38c
Paco <vyachhw@gmail.com>
2023-11-22 17:37:14 +0300

    Bearer JWT Added
    Теперь работа с аутентификацией и информацией пользователя происходит через json web token.

--- Todo.User.Api/Todo.User.Api/Controllers/UserController.cs
+++ Todo.User.Api/Todo.User.Api/Controllers/UserController.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Todo.User.Api.Interfaces;
 using Todo.User.Api.Models;
-using Todo.User.Api.Repositories;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Todo.User.Api.Controllers
 {
@@ -11,13 +15,22 @@ namespace Todo.User.Api.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly UserRepository userRepository;
+        private readonly IUserRepository userRepository;
         private readonly IMapper mapper;
+        private readonly IHttpContextAccessor contextAccessor;
+        private readonly IConfiguration configuration;
 
-        public UserController(UserRepository userRepository, IMapper mapper)
+        public UserController(
+            IUserRepository userRepository,
+            IMapper mapper,
+            IHttpContextAccessor contextAccessor,
+            IConfiguration configuration
+            )
         {
             this.userRepository = userRepository;
             this.mapper = mapper;
+            this.contextAccessor = contextAccessor;
+            this.configuration = configuration;
         }
 
         // GET: api/<UserController>
@@ -30,37 +43,71 @@ namespace Todo.User.Api.Controllers
 
         // GET api/<UserController>/5
         [HttpGet("{id}")]
-        public IActionResult Get(int id)
+        public IActionResult Get([FromHeader] string authorizationToken)
         {
-            var user = userRepository.Get(id);
-            return Ok(user);
+            return Ok(DecodeJwtToken(authorizationToken));
         }
 
         // POST api/<UserController>
         [HttpPost("register")]
         public IActionResult Post([FromBody] UserDto userDto)
         {
+            UserInfo userInfo = userRepository.Get(userDto.Login);
+            if (userInfo != null)
+                return BadRequest("Ошибка! Пользователь найден.");
+
             var user = mapper.Map<UserInfo>(userDto);
+
             user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
 
             userRepository.Post(user);
 
-            return Ok();
+            string token = CreateToken(user);
+            return Ok(token);
         }
 
         [HttpPost("login")]
         public IActionResult Login(UserDto userDto)
         {
+
+            if (userDto.Login == string.Empty || userDto.Password == string.Empty)
+                return BadRequest("Incorrect data");
+
             var user = userRepository.Get(userDto.Login);
-            return Ok(user);
+
+            if (userDto.Login != user.Login)
+                return BadRequest("User not found.");
+
+
+            if (!BCrypt.Net.BCrypt.Verify(userDto.Password, user.PasswordHash))
+                return BadRequest("Bad password.");
+
+
+            var refreshToken = GenerateRefreshToken();
+            SetRefreshToken(refreshToken, user);
+            string token = CreateToken(user);
+
+            return Ok(token);
         }
 
         // PUT api/<UserController>/5
         [HttpPut]
-        public IActionResult Put([FromBody] UserInfo user)
+        public IActionResult Put([FromBody] UserInfo userInfo)
         {
+            var user = userRepository.Get(userInfo.Login);
+
+            if (user == null)
+                return BadRequest("User not found.");
+
+            if (user.Login != userInfo.Login)
+                return BadRequest("User not found.");
+
+            user.Email = userInfo.Email;
+
             userRepository.Put(user);
-            return Ok("Пользователь обновлен.");
+
+            string token = CreateToken(user);
+            return Ok(token);
         }
 
         // DELETE api/<UserController>/5
@@ -69,6 +116,119 @@ namespace Todo.User.Api.Controllers
         {
             userRepository.Delete(id);
             return Ok();
+        }
+
+        [HttpGet("hasExpired")]
+        public IActionResult HasTokenExpired(string token)
+        {
+            var decodedToken = DecodeJwtToken(token);
+            if (!decodedToken.ContainsKey("exp"))
+            {
+                return BadRequest("Invalid Token.");
+            }
+
+            long expireDate = Convert.ToInt64(decodedToken["exp"]);
+
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(expireDate);
+
+            // Convert DateTimeOffset to DateTime if needed
+            DateTime dateTime = dateTimeOffset.LocalDateTime;
+
+            if (dateTime.Date > DateTime.Now)
+            {
+                return Ok(false);
+            }
+
+            return Ok(true);
+        }
+
+        /// <summary>
+        /// Генерирует объект обновления Refresh Token.
+        /// </summary>
+        /// <returns>Объект Refresh Token.</returns>
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddDays(7),
+                Created = DateTime.Now,
+            };
+            return refreshToken;
+        }
+
+        /// <summary>
+        /// Устанавливает Refresh Token для пользователя и создает соответствующий HTTP-кукис.
+        /// </summary>
+        /// <param name="newRefreshToken">Новый Refresh Token.</param>
+        /// <param name="user">Пользователь, для которого устанавливается Refresh Token.</param>
+        private void SetRefreshToken(RefreshToken newRefreshToken, UserInfo user)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires,
+            };
+            contextAccessor.HttpContext.Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+            user.TokenExpires = newRefreshToken.Expires;
+            user.TokenCreated = newRefreshToken.Created;
+            user.RefreshToken = newRefreshToken.Token;
+
+            userRepository.Put(user);
+        }
+
+        /// <summary>
+        /// Создает JWT-токен на основе данных пользователя.
+        /// </summary>
+        /// <param name="user">Пользователь, для которого создается токен.</param>
+        private string CreateToken(UserInfo user)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim("id", user.Id ?? string.Empty),
+                new Claim("name", user.Login),
+                new Claim("email", user.Email.ToString()),
+            };
+
+            //var tokenSettings = configuration.GetSection("AppSettings:Token").Value;
+            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings));
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+              .GetBytes(configuration
+              .GetSection("AppSettings:Token").Value!));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(7),
+                signingCredentials: cred
+                );
+            var result = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// Декодирует JWT-токен и возвращает его содержимое как словарь пар "тип-значение".
+        /// </summary>
+        /// <param name="token">JWT-токен для декодирования.</param>
+        /// <returns>Словарь, содержащий пары "тип-значение" из JWT-токена.</returns>
+        private static IDictionary<string, string> DecodeJwtToken(string token)
+        {
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var middle = token.Replace("bearer ", "");
+            var jwtToken = jwtHandler.ReadJwtToken(middle);
+
+            var claims = new Dictionary<string, string>();
+            foreach (var claim in jwtToken.Claims)
+            {
+                claims.Add(claim.Type, claim.Value);
+            }
+
+            return claims;
         }
     }
 }
--- Todo.User.Api/Todo.User.Api/Program.cs
+++ Todo.User.Api/Todo.User.Api/Program.cs
@@ -1,5 +1,9 @@
-using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 using Todo.User.Api.Data;
 using Todo.User.Api.Interfaces;
 using Todo.User.Api.Repositories;
@@ -15,13 +19,49 @@ builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyPolicy", builder =>
+    {
+        builder.WithOrigins("https://localhost:7097", "http://localhost:5250", "http://localhost:5080");
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+    });
+});
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Auth header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+             .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -32,9 +72,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("MyPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseStaticFiles();
 
 app.MapControllers();
 
